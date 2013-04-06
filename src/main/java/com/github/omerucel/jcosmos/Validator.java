42df58b6a2dbbc4d771c29edb19b112e91d043f4
Ömer ÜCEL <omerucel@gmail.com>
2013-04-06 03:24:30 +0300

    Validator sınıfının javax.validation ile çalışabilmesi sağlandı.

--- src/main/java/com/github/omerucel/jcosmos/Validator.java
+++ src/main/java/com/github/omerucel/jcosmos/Validator.java
@@ -1,5 +1,89 @@
 package com.github.omerucel.jcosmos;
 
-public class Validator extends ErrorManager{
+import com.github.omerucel.jcosmos.error.ValidationError;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import org.apache.commons.beanutils.BeanUtils;
 
+public class Validator<T> extends ErrorManager{
+    private static javax.validation.Validator validator;
+
+    public static class Form{
+        private String toCamelCase(String text)
+        {
+            String[] parts = text.split("_");
+            String camelCaseString = null;
+            for (String part : parts){
+                if (camelCaseString == null)
+                {
+                    camelCaseString = part;
+                    continue;
+                }
+
+                camelCaseString = camelCaseString + toProperCase(part);
+            }
+            return camelCaseString;
+        }
+
+        private String toProperCase(String s) {
+            return s.substring(0, 1).toUpperCase() +
+                       s.substring(1).toLowerCase();
+        }
+
+        public void bind(HashMap<Object, Object> data)
+        {
+            for(Map.Entry<Object, Object> entry : data.entrySet())
+            {
+                String key = toCamelCase((String) entry.getKey());
+                try {
+                    BeanUtils.setProperty(this, key, entry.getValue());
+                } catch (Exception ex) {
+                    continue;
+                }
+            }
+        }
+
+        public void validate()
+        {
+            
+        }
+    }
+
+    private void validate(T object)
+    {
+        Set<ConstraintViolation<T>> constraintViolations
+                = getValidator().validate(object);
+
+        if (!constraintViolations.isEmpty())
+        {
+            for(ConstraintViolation<T> violations : constraintViolations)
+            {
+                String fieldName = (String) violations.getInvalidValue();
+                String errorType = violations.getConstraintDescriptor()
+                        .getAnnotation().annotationType().getSimpleName();
+
+                addError(new ValidationError(
+                        fieldName, 
+                        errorType, 
+                        violations.getMessage()));
+            }
+        }
+    }
+
+    public final Boolean isValid(T object) {
+        validate(object);
+        ((Form)object).validate();
+        return !hasError();
+    }
+
+    private javax.validation.Validator getValidator()
+    {
+        if (validator == null)
+            validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        return validator;
+    }
 }
--- src/test/java/com/github/omerucel/jcosmos/ValidatorTest.java
+++ src/test/java/com/github/omerucel/jcosmos/ValidatorTest.java
@@ -1,13 +1,53 @@
 package com.github.omerucel.jcosmos;
 
+import java.util.HashMap;
+import javax.validation.constraints.NotNull;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ValidatorTest {
+    public static class Dummy extends Validator.Form{
+        @NotNull
+        public String email;
+
+        @NotNull
+        public String firstName;
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public void setFirstName(String firstName)
+        {
+            this.firstName = firstName;
+        }
+    }
+
     @Test
-    public void instance()
+    public void bind()
     {
-        Validator validator = new Validator();
-        assertTrue(validator instanceof ErrorManager);
+        HashMap<Object, Object> data = new HashMap<Object, Object>();
+        data.put("email", "test");
+        data.put("first_name", "test");
+
+        Dummy dummy = new Dummy();
+        dummy.bind(data);
+
+        assertEquals("test", dummy.email);
+        assertEquals("test", dummy.firstName);
+    }
+
+    @Test
+    public void isValid()
+    {
+        HashMap<Object, Object> data = new HashMap<Object, Object>();
+        data.put("email", "test");
+
+        Dummy dummy = new Dummy();
+        dummy.bind(data);
+
+        Validator<Dummy> validator = new Validator<Dummy>();
+        assertFalse(validator.isValid(dummy));
     }
 }
